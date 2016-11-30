66274562f9071c87abab74bfc796c796d095c2a9
Josh Desmond <Josh Desmond>
2016-11-30 16:55:16 -0500

    Got started with some toolbar and activity stuff

--- app/src/main/java/com/cs4518/halfway/MainActivity.java
+++ app/src/main/java/com/cs4518/halfway/MainActivity.java
@@ -1,13 +1,33 @@
 package com.cs4518.halfway;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_invite:
+                handleAddInvite();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
