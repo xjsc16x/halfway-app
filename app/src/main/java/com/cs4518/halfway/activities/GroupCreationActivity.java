9d2e38969d40d2a5b1f3674787a7b59c2d593c26
Josh Desmond <Josh Desmond>
2016-11-30 19:35:59 -0500

    Holy shit project isn't due yet screw this we're starting project 3 now

---
+++ app/src/main/java/com/cs4518/halfway/activities/GroupCreationActivity.java
@@ -0,0 +1,10 @@
+package com.cs4518.halfway.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 10 on 2016-11-30.
+ */
+public class GroupCreationActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/cs4518/halfway/activities/MainGroupListActivity.java
+++ app/src/main/java/com/cs4518/halfway/activities/MainGroupListActivity.java
@@ -1,5 +1,7 @@
-package com.cs4518.halfway;
+package com.cs4518.halfway.activities;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +9,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.cs4518.halfway.R;
 import com.cs4518.halfway.model.Group;
 
 import java.util.ArrayList;
@@ -15,18 +19,43 @@ import java.util.ArrayList;
 public class MainGroupListActivity extends AppCompatActivity {
 
 
-    /** This is the list of groups the activity should be displaying */
+    /**
+     * This is the list of groups the activity should be displaying
+     */
     ArrayList<Group> groupList;
     /** List Adapter which is used to show the data in a list */
     //FirebaseListAdapter<Group> fireAdapter;
+    /**
+     * Button to add new group
+     */
+    FloatingActionButton addNewGroupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ActionBar ab = getSupportActionBar();
         // ab.setDisplayHomeAsUpEnabled(true);
 
+        addNewGroupButton = (FloatingActionButton) findViewById(R.id.main_add_group_button);
+        addNewGroupButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        createGroup();
+                    }
+                }
+        );
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_group_list);
+    }
+
+    /**
+     * Method called when create group button is pressed
+     */
+    private void createGroup() {
+        Log.d("DEZ", String.format("Creating a group"));
+        Intent intent = new Intent(this, GroupCreationActivity.class);
+        startActivity(intent);
     }
 
     @Override
