00e8a6a9542d88c15c8b1dc31ec66227deb26441
Josh Desmond <Josh Desmond>
2016-12-12 22:26:32 -0500

    Some Javadoc on GroupViewHolder

--- app/src/main/java/com/cs4518/halfway/views/GroupViewHolder.java
+++ app/src/main/java/com/cs4518/halfway/views/GroupViewHolder.java
@@ -2,12 +2,18 @@ package com.cs4518.halfway.views;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+import com.cs4518.halfway.views.activities.GroupListFragment;
 import com.cs4518.halfway.R;
 import com.cs4518.halfway.model.Group;
 
+/**
+ * Custom {@link #ViewHolder} for Groups.
+ *
+ * @see GroupListFragment
+ * @see Group
+ */
 public class GroupViewHolder extends RecyclerView.ViewHolder {
     public TextView mTimeTextView;
     public TextView mDateTextView;
@@ -16,12 +22,17 @@ public class GroupViewHolder extends RecyclerView.ViewHolder {
 
     public GroupViewHolder(View itemView) {
         super(itemView);
-
+        
         mGroupNameTextView = (TextView) itemView.findViewById(R.id.group_name);
         mDateTextView = (TextView) itemView.findViewById(R.id.group_date);
         mTimeTextView = (TextView) itemView.findViewById(R.id.group_time);
     }
 
+    /**
+     * Updates this view's display to show the given group.
+     *
+     * @param mGroup Group for the view to display.
+     */
     public void bindGroup(Group mGroup) {
         mGroupNameTextView.setText(mGroup.groupName);
         mTimeTextView.setText(mGroup.meetingTime);
